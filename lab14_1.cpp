dbb04d8c0c17155bf37e56c65ee68bf96f895cba
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-28 16:30:44 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,21 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[]){
+    b[4] = b[5] = a[0];
+    for(int i = 0; i < n; i++)  b[0] += a[i];
+    b[0] /= n;
+
+    for(int i = 0; i < n; i++){
+        b[1] += pow((a[i] - b[0]), 2);
+        b[2] += log10(a[i]);
+        b[3] += 1/a[i];
+        b[4] = a[i]>b[4] ? a[i] : b[4];
+        b[5] = a[i]<b[5] ? a[i] : b[5];
+    }  
+    b[1] = sqrt(b[1]/n);
+    b[2] /= n;
+    b[2] = pow(10, b[2]);
+    b[3] = n/b[3];
+}
